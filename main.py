9fff38f4a3bfdc491c33c6e38b89fb135559b423
Tobias Machnitzki <tobias-machnitzki@web.de>
2021-04-28 15:14:19 +0200

    Add base model

---
+++ main.py
@@ -0,0 +1,78 @@
+import logging
+import os
+from argparse import ArgumentParser
+
+import pytorch_lightning as pl
+import torch
+from pytorch_lightning import loggers as pl_loggers
+from pytorch_lightning.callbacks import ModelCheckpoint
+from torchvision import transforms
+
+from src.models.LightningBaseModel import LightningModel
+from src.utils.DataLoader import DataLoader
+
+
+def load_config():
+    parser = ArgumentParser()
+    parser.add_argument('--seed', type=int, default=42)
+    parser.add_argument('--tensorboard_logger_logdir', type=str, default="logs/tb_logs")
+    parser.add_argument('--experiment_name', type=str, default="HIDA")
+    parser.add_argument('--checkpoint_file_path', type=str, default="logs/checkpoints")
+
+    args = parser.parse_args()
+    return args
+
+
+def main():
+
+    args = load_config()
+    torch.manual_seed(args.seed)
+    pl.seed_everything(args.seed)
+
+    if args.debug_mode:
+        logging.basicConfig(level=logging.DEBUG, format='%(name)s %(funcName)s %(levelname)s %(message)s')
+    else:
+        logging.basicConfig(level=logging.WARNING, format='%(name)s %(funcName)s %(levelname)s %(message)s')
+
+    if args.debug_mode:
+        torch.autograd.set_detect_anomaly(True)
+
+    transform = None
+
+    data_module = DataLoader.from_argparse_args(args, transform=transform)
+    data_module.setup()
+
+    # if the model is trained on GPU add a GPU logger to see GPU utilization in comet-ml logs:
+    if args.gpus == 0:
+        callbacks = None
+    else:
+        callbacks = [pl.callbacks.GPUStatsMonitor()]
+
+    # logging to tensorboard:
+    test_tube_logger = pl_loggers.TestTubeLogger(save_dir=args.tensorboard_logger_logdir,
+                                                 name=args.experiment_name,
+                                                 create_git_tag=False,
+                                                 log_graph=True)
+
+    # initializes a callback to save the 5 best model weights measured by the lowest loss:
+    checkpoint_callback = ModelCheckpoint(monitor="NLL Validation",
+                                          save_top_k=5,
+                                          mode='min',
+                                          save_last=True,
+                                          dirpath=os.path.join(args.checkpoint_file_path, args.experiment_name),
+                                          )
+
+    model = LightningModel(class_labels=data_module.unique_labels, **args.__dict__)
+
+    trainer = pl.Trainer.from_argparse_args(args,
+                                            callbacks=callbacks,
+                                            logger=[test_tube_logger],
+                                            checkpoint_callback=checkpoint_callback,
+                                            log_every_n_steps=10,
+                                            )
+
+    trainer.fit(model, data_module)
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/models/LightningBaseModel.py
@@ -0,0 +1,111 @@
+import logging
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pytorch_lightning as pl
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from sklearn.metrics import confusion_matrix
+from torchvision.models import resnet18
+import pytorch_lightning.metrics as pl_metrics
+
+
+class LightningModel(pl.LightningModule):
+
+    def __init__(self, class_labels, *args, **kwargs):
+
+        super().__init__()
+        self.class_labels = class_labels
+        self.model = self.define_model()
+        self.learning_rate = kwargs["learning_rate"]
+        self.loss_func = nn.BCELoss()
+        self.accuracy_func = pl_metrics.Accuracy()
+        self.save_hyperparameters()
+
+    def define_model(self):
+        return resnet18(pretrained=False, num_classes=2)
+
+    def forward(self, images,  *args, **kwargs):
+        predictions = self.model(images)
+        return F.log_softmax(predictions, dim=1)
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.model.parameters(), lr=self.hparams.learning_rate)
+        return optimizer
+
+    def training_step(self, batch, batch_idx, *args, **kwargs):
+        images, labels, label_names = batch
+
+        # for the toy example we will not use the meta_data and only the images to make a prediction.
+        predictions = self(images)
+        logging.debug(f"labels have the shape: {labels.shape}")
+        logging.debug(f"predictions have the shape: {predictions.shape}")
+
+
+        loss = self.loss_func(predictions, labels.view(-1).long())
+        accuracy = self.accuracy_func(F.softmax(predictions, dim=1).detach().cpu(), labels.to(torch.int).detach().cpu())
+
+        # lets log some values for inspection (for example in tensorboard):
+        self.log("NLL Training", loss)
+        self.log("Accuracy Training", accuracy)
+
+        return loss
+
+    def validation_step(self, batch, batch_idx, *args, **kwargs):
+        images, labels, label_names = batch
+
+        # for the toy example we will not use the meta_data and only the images to make a prediction.
+        predictions = self(images)
+
+        loss = self.loss_func(predictions, labels.view(-1).long())
+        accuracy = self.accuracy_func(F.softmax(predictions, dim=1).detach().cpu(), labels.to(torch.int).detach().cpu())
+
+        # lets log some values for inspection (for example in tensorboard):
+        self.log("NLL Validation", loss)
+        self.log("Accuracy Validation", accuracy)
+
+        if batch_idx == 0:
+            self.log_confusion_matrix(predictions, labels)
+            self.log_images(images, label_names)
+
+        return loss
+
+    def test_step(self, batch, batch_idx, *args, **kwargs):
+        images, labels, label_names = batch
+
+        # for the toy example we will not use the meta_data and only the images to make a prediction.
+        predictions = self(images)
+
+        loss = self.loss_func(predictions, labels.view(-1).long())
+        accuracy = self.accuracy_func(F.softmax(predictions, dim=1).detach().cpu(), labels.to(torch.int).detach().cpu())
+
+        # lets log some values for inspection (for example in tensorboard):
+        self.log("NLL Test", loss)
+        self.log("Accuracy Test", accuracy)
+
+        return loss
+
+    def log_confusion_matrix(self, predictions, targets):
+        conf_mat = confusion_matrix(torch.argmax(predictions, dim=-1).detach().cpu(), targets.detach().cpu(),
+                                    labels=np.arange(len(self.class_labels)))
+
+        fig, ax = plt.subplots()
+        ax.imshow(conf_mat)
+        ax.set_xticklabels(self.class_labels)
+        ax.set_yticklabels(self.class_labels)
+        ax.set_xlabel("Target")
+        ax.set_ylabel("Prediction")
+        self.logger.experiment[0].add_figure("Confusion_Matrix", fig, self.global_step)
+        plt.close("all")
+
+    def log_images(self, images, labels):
+        if self.hparams.batch_size >= 16:
+            fig, axes = plt.subplots(nrows=4, ncols=4, figsize=(12, 12))
+            for i in range(16):
+                ax = axes.flatten()[i]
+                ax.imshow(images[i].detach().cpu().moveaxis(0, -1))
+                ax.set_title(labels[i])
+
+            self.logger.experiment[0].add_figure("Image Matrix", fig, self.global_step)
+            plt.close("all")
